28655b0cd0dd853199a080a15603407b33eb30ae
MasaGratoR <masagrator@gmail.com>
2023-06-09 12:12:44 +0200

    Change DR approach

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -4,11 +4,13 @@
 nn::os::Tick lastFrameTick = nn::os::Tick(0);
 typedef int (*nvnGetPresentInterval_0)(void* nvnWindow);
 typedef int (*nvnSyncWait_0)(void* nvnSync, int64_t timeout);
+typedef uint64_t (*GetGpuTimeInNS)(void* _this);
 void* nvnSyncWait_ptr = 0;
 void* nvnWindowGetPresentInterval_ptr = 0;
 void* nvnWindowSync = 0;
+void* GetGpuTimeInNS_ptr = 0;
 bool cutsceneFlag = false;
-float frameTime = 0;
+float frameTime = (1.0/30);
 float* windGrassFactorPtr = 0;
 int presentInterval = 2;
 float deltaMax = (1.0/30);
@@ -17,16 +19,7 @@ HOOK_DEFINE_TRAMPOLINE(SyncWait) {
 
 	static void* Callback(uint64_t _this) {
 		nvnWindowSync = (void*)(_this + 0xFD0);
-		uint64_t frameTimeTemp = 0;
-		if (lastFrameTick != nn::os::Tick(0)) {
-			nn::os::Tick tick = nn::os::GetSystemTick();
-			frameTimeTemp = nn::os::ConvertToTimeSpan(tick - lastFrameTick).GetNanoSeconds();
-			if (frameTimeTemp > 50000000) {
-				frameTime = (1.0/20);
-			}
-			else frameTime = (float)frameTimeTemp / 1000000000;
-		}
-		if ((cutsceneFlag || frameTimeTemp < (10000000 * (uint64_t)presentInterval)) && nvnWindowSync) {
+		if (cutsceneFlag && nvnWindowSync) {
 			((nvnSyncWait_0)(nvnSyncWait_ptr))(nvnWindowSync, -1);
 		}
 		return Orig(_this);
@@ -36,70 +29,27 @@ HOOK_DEFINE_TRAMPOLINE(SyncWait) {
 HOOK_DEFINE_TRAMPOLINE(EndFramebuffer) {
 
 	static void* Callback(uint64_t _this) {
-
-		void* ret = Orig(_this);
 		
-		nn::os::Tick newFrameTick = nn::os::GetSystemTick();
-		if (lastFrameTick != nn::os::Tick(0)) {
-			int64_t deltaTimeInNs = nn::os::ConvertToTimeSpan(newFrameTick - lastFrameTick).GetNanoSeconds();
-			float deltaTimeInS = (float)deltaTimeInNs / 1000000000;
-			lastFrameTick = newFrameTick;
+		uint64_t RenderStruct = *(uint64_t*)(_this + 0x5028);
 
-			uint64_t RenderStruct = *(uint64_t*)(_this + 0x5028);
-			void* nvnWindow = (void*)(RenderStruct + 0x21A0);
+		void* nvnWindow = (void*)(RenderStruct + 0x21A0);
 
-			presentInterval = ((nvnGetPresentInterval_0)(nvnWindowGetPresentInterval_ptr))(nvnWindow);
-			int* vSync = (int*)exl::util::modules::GetTargetOffset(0xB74BF0);
-			int* vSync_old = (int*)exl::util::modules::GetTargetOffset(0xB74BF4);
+		presentInterval = ((nvnGetPresentInterval_0)(nvnWindowGetPresentInterval_ptr))(nvnWindow);
+		int* vSync = (int*)exl::util::modules::GetTargetOffset(0xB74BF0);
+		int* vSync_old = (int*)exl::util::modules::GetTargetOffset(0xB74BF4);
 
-			cutsceneFlag = *(bool*)exl::util::modules::GetTargetOffset(0xBD7F40);
-			if (!cutsceneFlag) {
-				*vSync = presentInterval;
-				*vSync_old = presentInterval;
-			}
-			else {
-				*vSync = 2;
-				*vSync_old = 2;
-				presentInterval = 2;
-			}
-
-			deltaMax = 1.0/30;
-			float deltaMin = 1.0/20;
-			if (presentInterval == 1) {
-				deltaMax = 1.0/60;
-			}
-			else if (presentInterval == 0) {
-				deltaMax = 1.0/120;
-			}
-			
-			float gameSpeed = deltaMax;
-			if (!cutsceneFlag) {
-				if (deltaTimeInS > deltaMin) {
-					gameSpeed = deltaMin;
-				}
-				else if (deltaTimeInS > deltaMax) {
-					gameSpeed = deltaTimeInS;
-				}
-			}
-
-			uintptr_t gameStruct = *(uintptr_t*)exl::util::modules::GetTargetOffset(0xB8F0D0);
-			uintptr_t uiStruct = *(uintptr_t*)exl::util::modules::GetTargetOffset(0xC222C8);
-			if (gameStruct) {
-				float* gameSpeedPtr1 = (float*)(gameStruct + 0x78);
-				float* gameSpeedPtr2 = (float*)(gameStruct + 0x7C);
-				*windGrassFactorPtr = gameSpeed;
-				*gameSpeedPtr1 = gameSpeed;
-				*gameSpeedPtr2 = gameSpeed;
-				
-			}
-			if (uiStruct) {
-				float* uiSpeedPtr = (float*)(uiStruct + 0x80);
-				*uiSpeedPtr = gameSpeed;
-			}
+		cutsceneFlag = *(bool*)exl::util::modules::GetTargetOffset(0xBD7F40);
+		if (!cutsceneFlag) {
+			*vSync = presentInterval;
+			*vSync_old = presentInterval;
 		}
-		else lastFrameTick = newFrameTick;
+		else {
+			*vSync = 2;
+			*vSync_old = 2;
+			presentInterval = 2;
+		}
 		
-		return ret;
+		return Orig(_this);
 
 	}
 
@@ -107,15 +57,68 @@ HOOK_DEFINE_TRAMPOLINE(EndFramebuffer) {
 
 HOOK_DEFINE_TRAMPOLINE(GetGpuTime) {
 	static float Callback(void) {
-		if (!cutsceneFlag && presentInterval < 2) {
+		if (!cutsceneFlag && (presentInterval < 2) && (frameTime > (1.003 * deltaMax))) {
 			// Original calculation = ((frameTimeGPU / deltaMax) * 100) + 3;
-			float Scale = ((frameTime / deltaMax) * 110) + 3;
-			return Scale;
+			return ((frameTime / deltaMax) * 100.0);
 		}
-		else return Orig();
+		else {
+			void* struct_this = *(void**)exl::util::modules::GetTargetOffset(0xECCEC0);
+			uint64_t nanoseconds = ((GetGpuTimeInNS)(GetGpuTimeInNS_ptr))(struct_this);
+			float seconds = nanoseconds / 1000000000;
+			return ((seconds / deltaMax) * 100.0) + 3.0;
+		}
 	}
 };
 
+HOOK_DEFINE_INLINE(GetCpuTime) {
+	static void Callback(exl::hook::nx64::InlineCtx* ctx) {
+		
+		nn::os::Tick tick = nn::os::GetSystemTick();
+
+		if (lastFrameTick != nn::os::Tick(0)) {
+			uint64_t frameTimeTemp = nn::os::ConvertToTimeSpan(tick - lastFrameTick).GetNanoSeconds();
+			if (frameTimeTemp > 50000000) {
+				frameTime = (1.0/20);
+			}
+			else frameTime = (float)frameTimeTemp / 1000000000;
+		}
+		lastFrameTick = tick;
+
+		deltaMax = 1.0/30;
+		float deltaMin = 1.0/20;
+		if (presentInterval == 1) {
+			deltaMax = 1.0/60;
+		}
+		else if (presentInterval == 0) {
+			deltaMax = 1.0/120;
+		}
+		
+		float gameSpeed = deltaMax;
+		if (!cutsceneFlag) {
+			if (frameTime > deltaMin) {
+				gameSpeed = deltaMin;
+			}
+			else if (frameTime > deltaMax) {
+				gameSpeed = frameTime;
+			}
+		}
+
+		uintptr_t gameStruct = *(uintptr_t*)exl::util::modules::GetTargetOffset(0xB8F0D0);
+		uintptr_t uiStruct = *(uintptr_t*)exl::util::modules::GetTargetOffset(0xC222C8);
+		if (gameStruct) {
+			float* gameSpeedPtr1 = (float*)(gameStruct + 0x78);
+			float* gameSpeedPtr2 = (float*)(gameStruct + 0x7C);
+			*windGrassFactorPtr = gameSpeed;
+			*gameSpeedPtr1 = gameSpeed;
+			*gameSpeedPtr2 = gameSpeed;
+			
+		}
+		if (uiStruct) {
+			float* uiSpeedPtr = (float*)(uiStruct + 0x80);
+			*uiSpeedPtr = gameSpeed;
+		}
+	}
+};
 
 extern "C" void exl_main(void* x0, void* x1) {
 	/* Setup hooking enviroment. */
@@ -127,6 +130,7 @@ extern "C" void exl_main(void* x0, void* x1) {
 	nvnWindowGetPresentInterval_ptr = (void*)(exl::util::GetSdkModuleInfo().m_Total.m_Start + 0x2CAB68);
 
 	GetGpuTime::InstallAtOffset(0x6A3084);
+	GetCpuTime::InstallAtOffset(0x700164);
 
 	// Wind speed factor from MAIN+0x15EC500
 	exl::patch::CodePatcher p(0x747DD8);
@@ -135,6 +139,8 @@ extern "C" void exl_main(void* x0, void* x1) {
 
 	windGrassFactorPtr = (float*)exl::util::modules::GetTargetOffset(0x15EC500);
 	*windGrassFactorPtr = (1.0/30);
+
+	GetGpuTimeInNS_ptr = (void*)exl::util::modules::GetTargetOffset(0x6F489C);
 }
 
 extern "C" NORETURN void exl_exception_entry() {
